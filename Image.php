44e6eea28c23bafc0d694d3d048630f81eebe488
ITDevv <aidonline01@gmail.com>
2021-07-01 21:43:06 +0000

    resize, fit, crop, webp done

---
+++ Image.php
@@ -0,0 +1,78 @@
+<?php
+
+class Image {
+    private $source;
+    public $info;
+
+    public function load($path) {
+        $this->path = $path;
+
+        if(!file_exists($this->path)) return false;
+
+        $this->source = new Imagick($this->path);
+        $this->info = $this->getInfo();
+
+        return true;
+    }
+
+    private function getInfo() {
+        $info = [];
+        $info['ext'] = strToLower($this->source->getImageFormat());
+        if($info['ext'] === 'jpeg') $info['ext'] = 'jpg';
+
+        $info['width'] = $this->source->getImageWidth();
+        $info['height'] = $this->source->getImageHeight();
+        
+        return $info;
+    }
+
+    // resize image to exact size
+    public function resize($width, $height) {
+        $this->source->adaptiveResizeImage($width, $height);
+    }
+
+    // crop image to specified area
+    public function crop($width, $height, $xPos = 'center', $yPos = 'center') {
+        $cropWidth = $width;
+        $cropHeight = $height;
+        $originalAspect = $this->info['width'] / $this->info['height'];
+        $fitAspect = $width / $height;
+
+        // get image minimal dimension
+        if($originalAspect < $fitAspect) $height = (int)($width / $this->info['width'] * $this->info['height']);
+        else $width = (int)($height / $this->info['height'] * $this->info['width']);
+
+        // get x, y
+        if($xPos === 'left') $x = 0;
+        if($xPos === 'center') $x = ($width - $cropWidth) / 2;
+        if($xPos === 'right') $x = $width - $cropWidth;
+
+        if($yPos === 'top') $y = 0;
+        if($yPos === 'center') $y = ($height - $cropHeight) / 2;
+        if($yPos === 'bottom') $y = $height - $cropHeight;
+
+        $this->source->adaptiveResizeImage($width, $height);
+        $this->source->cropImage($cropWidth, $cropHeight, $x, $y);
+    }
+
+    // resize image to area without changing ratio
+    public function fit($width, $height) {
+        if($width < $this->info['width']) {
+            $height = (int)($width / $this->info['width'] * $this->info['height']); 
+        }
+
+        if($height < $this->info['height']) {
+            $width = (int)($height / $this->info['height'] * $this->info['width']); 
+        }
+
+        $this->source->adaptiveResizeImage($width, $height);
+    }
+
+    public function convertWebp() {
+
+    }
+
+    public function save($path, $type = 'png', $quality = 0) {
+        $this->source->writeImage("$type:$path");
+    }
+}
---
+++ example/run.php
@@ -0,0 +1,44 @@
+<?php
+error_reporting(E_STRICT);
+
+include __DIR__ . '/../Image.php';
+
+$path = __DIR__ . '/source.jpg';
+$dest = __DIR__ . '/dist/dest';
+
+cropImage($path, $dest);
+fitImage($path, $dest);
+resizeImage($path, $dest);
+
+function cropImage($path, $dest) {
+    $image = new Image();
+
+    if(!$image->load($path)) die('Image doesn\'t exist');
+    
+    $image->crop(400, 400, 'center', 'center');
+    
+    $image->save("$dest.crop.jpg", 'jpg');
+    $image->save("$dest.crop.jpg.webp", 'webp');
+}
+
+function fitImage($path, $dest) {
+    $image = new Image();
+
+    if(!$image->load($path)) die('Image doesn\'t exist');
+    
+    $image->fit(400, 400, 'center', 'center');
+    
+    $image->save("$dest.fit.jpg", 'jpg');
+    $image->save("$dest.fit.jpg.webp", 'webp');
+}
+
+function resizeImage($path, $dest) {
+    $image = new Image();
+
+    if(!$image->load($path)) die('Image doesn\'t exist');
+    
+    $image->resize(400, 400, 'center', 'center');
+    
+    $image->save("$dest.resize.jpg", 'jpg');
+    $image->save("$dest.resize.jpg.webp", 'webp');
+}
